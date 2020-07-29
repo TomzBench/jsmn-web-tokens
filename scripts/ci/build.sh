1778710539e0af4e86b34ba5c957cf48ccd25d79
thomas <thomas.chiantia@gmail.com>
2020-07-29 08:51:35 -0400

    fixed travis?

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ $GENERATOR == "Visual Studio 15 2017 Win64" ]; then
+if [ "$GENERATOR" == "Visual Studio 15 2017 Win64" ]; then
+	echo "[build.sh] - Windows Generator [Visual Studio 15 2017 Win64]"
 	(
 		cmake \
 			-G "$GENERATOR" \
@@ -12,7 +13,22 @@ if [ $GENERATOR == "Visual Studio 15 2017 Win64" ]; then
 		
 		cmake --build . --target install
 	) || exit 1
+elif [ "$GENERATOR" == "Visual Studio 16 2019" ]; then
+	echo "[build.sh] - Windows Generator [Visual Studio 16 2019]"
+	(
+		cmake \
+			-G "$GENERATOR" \
+			-A "x64" \
+			-DCMAKE_GENERATOR_PLATFORM=x64 \
+			-DCMAKE_INSTALL_PREFIX=./build/install \
+			-DWITH_CRYPTO=$WITH_CRYPTO \
+			-DWITH_SYSTEM_DEPENDENCIES=$WITH_SYSTEM_DEPENDENCIES \
+			-DENABLE_TESTING=$ENABLE_TESTING .
+		
+		cmake --build . --target install
+	) || exit 1
 else
+	echo "[build.sh] - Building for linux"
 	(
 		cmake \
 			-DCMAKE_INSTALL_PREFIX=./build/install \

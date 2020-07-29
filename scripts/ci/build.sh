3bf020b6d1f7b8b71961d6d38e7c195a191e0b2b
thomas <thomas.chiantia@gmail.com>
2020-07-29 08:31:05 -0400

    fixed travis?

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-(
-	cmake \
-		-DCMAKE_GENERATOR_PLATFORM=x64 \
-		-DCMAKE_INSTALL_PREFIX=./build/install \
-		-DWITH_CRYPTO=$WITH_CRYPTO \
-		-DWITH_SYSTEM_DEPENDENCIES=$WITH_SYSTEM_DEPENDENCIES \
-		-DENABLE_TESTING=$ENABLE_TESTING .
-	
-	cmake --build . --target install
-) || exit 1
+if [ $GENERATOR = "Visual Studio 15 2017 Win64" ]; then
+	(
+		cmake \
+			-G "$GENERATOR" \
+			-DCMAKE_GENERATOR_PLATFORM=x64 \
+			-DCMAKE_INSTALL_PREFIX=./build/install \
+			-DWITH_CRYPTO=$WITH_CRYPTO \
+			-DWITH_SYSTEM_DEPENDENCIES=$WITH_SYSTEM_DEPENDENCIES \
+			-DENABLE_TESTING=$ENABLE_TESTING .
+		
+		cmake --build . --target install
+	) || exit 1
+else
+	(
+		cmake \
+			-DCMAKE_INSTALL_PREFIX=./build/install \
+			-DWITH_CRYPTO=$WITH_CRYPTO \
+			-DWITH_SYSTEM_DEPENDENCIES=$WITH_SYSTEM_DEPENDENCIES \
+			-DENABLE_TESTING=$ENABLE_TESTING .
+		
+		cmake --build . --target install
+	) || exit 1
+fi
 
 if [ $ENABLE_TESTING ]; then
 	ctest -T memcheck &&

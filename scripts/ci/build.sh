844d5110d7769eeafe89c36695e7fbefb48e508f
thomas <thomas.chiantia@gmail.com>
2020-07-29 08:20:33 -0400

    merge dev

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-(cmake \
-	-DCMAKE_INSTALL_PREFIX=./build/install \
-	-DWITH_CRYPTO=$WITH_CRYPTO \
-	-DWITH_SYSTEM_DEPENDENCIES=$WITH_SYSTEM_DEPENDENCIES \
-	-DENABLE_TESTING=$ENABLE_TESTING .
+(
+	cmake \
+		-DCMAKE_GENERATOR_PLATFORM=x64 \
+		-DCMAKE_INSTALL_PREFIX=./build/install \
+		-DWITH_CRYPTO=$WITH_CRYPTO \
+		-DWITH_SYSTEM_DEPENDENCIES=$WITH_SYSTEM_DEPENDENCIES \
+		-DENABLE_TESTING=$ENABLE_TESTING .
+	
+	cmake --build . --target install
 ) || exit 1
 
-(cmake --build . --target install || exit 1)
-
 if [ $ENABLE_TESTING ]; then
-	(ctest -T memcheck || exit 1)
-	(make test-coverage || exit 1)
-	(bash <(curl -s https://codecov.io/bash) -f test-coverage.info || exit 1)
+	ctest -T memcheck &&
+		make test-coverage &&
+		bash <(curl -s https://codecov.io/bash) -f test-coverage.info;
 fi
